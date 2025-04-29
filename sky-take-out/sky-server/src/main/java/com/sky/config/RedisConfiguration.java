ec783f5f74fd4de20bdba3efcf18ea0e0d23e723
yihaitao <yihaitao@126.com>
2025-04-30 00:02:01 +0800

    增加redis缓存菜品

--- sky-take-out/sky-server/src/main/java/com/sky/config/RedisConfiguration.java
+++ sky-take-out/sky-server/src/main/java/com/sky/config/RedisConfiguration.java
@@ -1,10 +1,17 @@
 package com.sky.config;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
@@ -13,11 +20,38 @@ public class RedisConfiguration {
     @Bean
     public RedisTemplate redisTemplate(RedisConnectionFactory redisConnectionFactory){
         log.info("开始创建redis模板对象...");
-        RedisTemplate redisTemplate = new RedisTemplate();
+        /*RedisTemplate redisTemplate = new RedisTemplate();
         //设置redis的连接工厂对象
         redisTemplate.setConnectionFactory(redisConnectionFactory);
         //设置redis key的序列化器
         redisTemplate.setKeySerializer(new StringRedisSerializer());
-        return redisTemplate;
+        return  redisTemplate;*/
+
+        //1.创建 redisTemplate
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        //2.设置连接工厂
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        //3.设置序列化工具
+        //key 和 hashkey 采用 String 序列化
+        redisTemplate.setKeySerializer(RedisSerializer.string());
+        redisTemplate.setHashKeySerializer(RedisSerializer.string());
+        //value 和 hashvalue 采用 json 序列化
+        GenericJackson2JsonRedisSerializer jsonRedisSerializer = new GenericJackson2JsonRedisSerializer();
+        redisTemplate.setValueSerializer(jsonRedisSerializer);
+        redisTemplate.setHashValueSerializer(jsonRedisSerializer);
+
+        // 使用 Jackson2JsonRedisSerializer 序列化 value
+//        Jackson2JsonRedisSerializer<Object> serializer = new Jackson2JsonRedisSerializer<>(Object.class);
+//        ObjectMapper objectMapper = new ObjectMapper();
+////        objectMapper.activateDefaultTyping(
+////                LaissezFaireSubTypeValidator.instance,
+////                ObjectMapper.DefaultTyping.NON_FINAL,
+////                JsonTypeInfo.As.PROPERTY
+////        );
+//        objectMapper.registerModule(new JavaTimeModule());
+//        serializer.setObjectMapper(objectMapper);
+//        redisTemplate.setValueSerializer(serializer);
+//        redisTemplate.setHashValueSerializer(serializer);
+        return  redisTemplate;
     }
 }
--- sky-take-out/sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-take-out/sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -1,5 +1,8 @@
 package com.sky.controller.user;
 
+import com.alibaba.fastjson.JSON;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sky.constant.StatusConstant;
 import com.sky.entity.Dish;
 import com.sky.result.Result;
@@ -9,10 +12,14 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 @RestController("userDishController")
 @RequestMapping("/user/dish")
@@ -21,7 +28,8 @@ import java.util.List;
 public class DishController {
     @Autowired
     private DishService dishService;
-
+    @Autowired
+    private RedisTemplate redisTemplate;
     /**
      * 根据分类id查询菜品
      *
@@ -30,13 +38,31 @@ public class DishController {
      */
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
-    public Result<List<DishVO>> list(Long categoryId) {
+    public Result<List<DishVO>> list(Long categoryId) throws JsonProcessingException {
+        //构造redis中的key，规则：dish_分类id
+        String key = "dish_" + categoryId;
+
+        //查询redis中是否存在菜品数据
+//        List<DishVO> list = (List<DishVO>) redisTemplate.opsForValue().get(key);
+        String res = (String)redisTemplate.opsForValue().get(key);
+        List<DishVO> list = JSON.parseArray(res,DishVO.class);
+//        List<DishVO> dataList = (List<DishVO>)redisTemplate.opsForList().range("list_"+key, 0, -1); // 获取列表中的所有元素
+        if(list != null && list.size() > 0){
+            //如果存在，直接返回，无须查询数据库
+            return Result.success(list);
+        }
+        ////////////////////////////////////////////////////////
         Dish dish = new Dish();
         dish.setCategoryId(categoryId);
         dish.setStatus(StatusConstant.ENABLE);//查询起售中的菜品
 
-        List<DishVO> list = dishService.listWithFlavor(dish);
-
+        //如果不存在，查询数据库，将查询到的数据放入redis中
+        list = dishService.listWithFlavor(dish);
+        ////////////////////////////////////////////////////////
+        ObjectMapper mapper = new ObjectMapper();
+//        redisTemplate.opsForValue().set(key,list);
+        redisTemplate.opsForValue().set(key,JSON.toJSONString(list));
+//        redisTemplate.opsForList().rightPushAll("list_"+key, list);
         return Result.success(list);
     }
 
